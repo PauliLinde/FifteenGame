145021fac4ba3da1aaab6d6ba4900714dc5f6882
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2024-10-30 21:29:09 +0100

    Metod shuffleTiles, validMove, countMoves

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,20 +14,40 @@ public class Logic {
             tilesList.add(i);
         }
     }
-    //Scramble metod
+    //Shuffle metod
+    public List shuffleTiles() {
+        if (tilesList != null) {
+            Collections.shuffle(tilesList);
+        }
+        return tilesList;
+    }
+
+    //Metod för att kolla att draget är möjligt
+    public boolean validMove(int pushed, int empty) {
+        if (pushed == empty - 1 && pushed % 4 != 0||
+                pushed == empty + 1 && pushed % 4 != 3 ||
+                pushed == empty - 4 && pushed >= 4 ||
+                pushed == empty + 4 && pushed <= 11) {
+            return true;
+        }
+        return false;
+    }
 
     //Metod för speldrag (knapptryckning)
     public void moveTile(int pushed, int empty) {
-            if (pushed == empty - 1 && pushed % 4 != 0||
-                    pushed == empty + 1 && pushed % 4 != 3 ||
-                    pushed == empty - 4 && pushed >= 4 ||
-                    pushed == empty + 4 && pushed <= 11) {
-                //Move tile!
-            }
+        if (validMove(pushed, empty)) {
+
+            countMoves();
+        }
     }
 
     //Metod för redovisning (Snabb vinst)
 
-    //Metod för counter av speldrag(knapptryckningar)
+
+    //Metod för counter av speldrag (Vill vi sära på den från moveTile?
+    // Går ju att lägga counter++ i moveTile-metoden. Men separation of concern osv?)
+    public void countMoves(){
+        counter++;
+    }
 
 }
