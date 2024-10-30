ce257e666e6f2cf5a43c16624cd9911659554378
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2024-10-30 14:29:32 +0100

    constructor, moveTile first draft

--- src/Logic.java
+++ src/Logic.java
@@ -1,18 +1,29 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Logic {
-    List<Integer> tiles = new LinkedList<Integer>();
-    String output = "Du vann!"; //Hårdkodning?
+    List<Integer> tilesList = new LinkedList<>();
     int counter = 0;
+    //Möjligen outputvariabel?
 
 
+    //Konstruktor som initierar listan
+    public Logic() {
+        for (int i = 0; i <= 15; i++) {
+            tilesList.add(i);
+        }
+    }
     //Scramble metod
 
     //Metod för speldrag (knapptryckning)
-
-    //Metod för vinst? (Onödigt?)
+    public void moveTile(int pushed, int empty) {
+            if (pushed == empty - 1 && pushed % 4 != 0||
+                    pushed == empty + 1 && pushed % 4 != 3 ||
+                    pushed == empty - 4 && pushed >= 4 ||
+                    pushed == empty + 4 && pushed <= 11) {
+                //Move tile!
+            }
+    }
 
     //Metod för redovisning (Snabb vinst)
 
