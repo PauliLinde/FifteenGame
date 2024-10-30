672729af2d36e9de17cf53da004baaefb00a6ff4
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2024-10-30 13:10:48 +0100

    First draft, class framework

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,21 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Logic {
-    //Testing
+    List<Integer> tiles = new LinkedList<Integer>();
+    String output = "Du vann!"; //Hårdkodning?
+    int counter = 0;
+
+
+    //Scramble metod
+
+    //Metod för speldrag (knapptryckning)
+
+    //Metod för vinst? (Onödigt?)
+
+    //Metod för redovisning (Snabb vinst)
+
+    //Metod för counter av speldrag(knapptryckningar)
+
 }
