a56bb31d933f57bc20dab23ee50f449140138487
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-10-31 15:18:38 +0100

    1: Lagt till solution-list. 2: Lagt metod checkWinning. 3: Ändrat empty från 0 till 16. 4: Ändrat i moveTile(); bytt tidigare kod mot Collection.swap.

--- src/Logic.java
+++ src/Logic.java
@@ -1,7 +1,6 @@
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Comparator;
 
 /////////////////////////////////////////////////////////////
 ////// Metoder kvar: För redovisning (Snabb vinst) /////////
