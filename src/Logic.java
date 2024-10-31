5559a85ea2e7cbab2fe567f2ab2fd9f4d73b41dd
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-10-31 16:20:03 +0100

    1: Lagt till konstruktor för easyWin. 2: Kommenterat vilka metoder jag testat logik i. 3: Getter till solutionList

--- src/Logic.java
+++ src/Logic.java
@@ -12,33 +12,45 @@ import java.util.List;
 public class Logic {
     private final List<Integer> tilesList = new LinkedList<>();
     private final List<Integer> solutionList = new LinkedList<>();
+
     private int counter = 0;
     //Möjligen outputvariabel?
 
 
     //Konstruktor initierar tileslistan och facitlistan och shufflar tileslistan
-    //16 är tom ruta
+    //16 är tom ruta        Testat! /Y
     public Logic() {
         for (int i = 1; i <= 16; i++) {
             tilesList.add(i);
             solutionList.add(i);
         }
-
         shuffleTiles();
     }
+    //Konstruktor för easy win
+    public Logic(boolean easyWin) {
+        for (int i = 1; i <= 16; i++) {
+            tilesList.add(i);
+            solutionList.add(i);
+        }
+        if (easyWin) {
+            Collections.swap(tilesList, 14 , 15 );
+        }else
+            shuffleTiles();
+    }
 
-    //Shuffle metod
+    //Shuffle metod         Testat! /Y
     public void shuffleTiles() {
         Collections.shuffle(tilesList);
     }
 
-    //Metod för att hitta index av tomma rutan
+    //Metod för att hitta index av tomma rutan      Testat! /Y
     public int findEmptyTile() {
         return tilesList.indexOf(16);
     }
 
-    //Metod för att kolla att draget är möjligt
+    //Metod för att kolla att draget är möjlig
     public boolean validMove(int pushed, int empty) {
+
         if (pushed == empty - 1 && pushed % 4 != 0||
                 pushed == empty + 1 && pushed % 4 != 3 ||
                 pushed == empty - 4 && pushed >= 4 ||
@@ -55,13 +67,14 @@ public class Logic {
             int indexPushed = tilesList.indexOf(pushed);
             int indexEmpty = tilesList.indexOf(empty);
             //Ersatte tidigare rader med metod swap för att jag blev osäker på om det fungerade med de tidigare raderna efter varandra
+
             Collections.swap(tilesList, indexPushed, indexEmpty);
 
             countMoves();
         } //Här skulle kod för hantering av felaktigt drag kunna skrivas
     }
 
-    //Metod för att kolla vinst, returnerar boolean
+    //Metod för att kolla vinst, returnerar boolean     Testat! /Y
     public boolean checkWinning(){
         boolean won = false;
 
@@ -84,5 +97,9 @@ public class Logic {
     //För inkapsling
     public List<Integer> getTilesList() {
         return tilesList;
+    }
+    //För testning
+    public List<Integer> getSolutionList() {
+        return solutionList;
     }
 }
