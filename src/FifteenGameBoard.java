a6bbb74a40be4324c466858b16645f07e9515675
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-11-01 16:57:35 +0100

    Skapar JButton med forloop istället.

--- src/FifteenGameBoard.java
+++ src/FifteenGameBoard.java
@@ -13,13 +13,6 @@ public class FifteenGameBoard extends JFrame {
     JPanel buttonPanel = new JPanel();
     JPanel southPanel = new JPanel();
 
-    JButton oneButton = new JButton(), twoButton = new JButton(), threeButton = new JButton(), fourButton = new JButton(),
-        fiveButton = new JButton(), sixButton = new JButton(), sevenButton = new JButton(), eightButton = new JButton(),
-        nineButton = new JButton(), tenButton = new JButton(), elevenButton = new JButton(), twelveButton = new JButton(),
-        thirteenButton = new JButton(), fourteenButton = new JButton(), fifteenButton = new JButton(), emptyButton = new JButton();
-
-    ActionListener moveActionListner;
-
     JButton newGameButton = new JButton("New Game");
 
     JLabel winnerLabel = new JLabel("You won!");
@@ -37,10 +30,10 @@ public class FifteenGameBoard extends JFrame {
         panel.add(buttonPanel, BorderLayout.CENTER);
         panel.add(southPanel, BorderLayout.SOUTH);
 
-        buttons.add(oneButton); buttons.add(twoButton); buttons.add(threeButton); buttons.add(fourButton);
-        buttons.add(fiveButton); buttons.add(sixButton); buttons.add(sevenButton); buttons.add(eightButton);
-        buttons.add(nineButton); buttons.add(tenButton); buttons.add(elevenButton); buttons.add(twelveButton);
-        buttons.add(thirteenButton); buttons.add(fourteenButton); buttons.add(fifteenButton); buttons.add(emptyButton);
+        for (int i = 0; i < 16; i ++){
+            JButton newButton = new JButton();
+            buttons.add(newButton);
+        }
 
         buttonPanel.setLayout(new GridLayout(4, 4));
         buttonPanel.setPreferredSize(new Dimension(500, 500));
