b14d035eed9db1e06d7e7e58087335c1de18d9a4
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-11-02 16:15:12 +0100

    Metod för solvable

--- src/Logic.java
+++ src/Logic.java
@@ -3,6 +3,14 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+
+/*
+    OBS: Denna klass innehåller metoden public boolean checkSolvable() innehåller kod som bygger på kod och logik
+    hämtad från Geeks for Geeks: How to check if an instance puzzle is solvable
+    https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+    och Claude, en AI-assistant från Anthropic (conversation date: November 2, 2024)
+    Datum: 2024-11-02
+*/
 public class Logic {
 
     private final List<Tile> tilesList = new LinkedList<>();
@@ -40,7 +48,9 @@ public class Logic {
     }
 
     public void shuffleTiles() {
-        Collections.shuffle(tilesList);
+        do {
+            Collections.shuffle(tilesList);
+        }while (!checkSolvable());
     }
 
     public int findEmptyTile() {
@@ -81,6 +91,48 @@ public class Logic {
             won = true;
         }
         return won;
+    }
+
+    /*
+        OBS: Denna metod innehåller kod som bygger på kod och logik
+        hämtad från Geeks for Geeks: How to check if an instance puzzle is solvable
+        https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+        och Claude, en AI-assistant från Anthropic (conversation date: November 2, 2024)
+        Datum: 2024-11-02
+    */
+    public boolean checkSolvable(){
+        boolean isSolvable = false;
+        int[] solvable = new int[16];
+        int indexEmpty = findEmptyTile();
+        int inversions = 0;
+
+        for (int i = 0; i < solvable.length; i++) {
+            Tile tile = tilesList.get(i);
+            solvable[i] = tile.getTileNumber();
+        }
+
+        for (int i = 0; i < 15; i++){
+            for (int j = i + 1; j < 16; j++){
+
+                if (solvable[j] !=16 && solvable[i] != 16
+                && solvable[i] > solvable[j])
+                    inversions++;
+            }
+        }
+
+        int rowFromBottom = 3 - (indexEmpty/4);
+        boolean even = (rowFromBottom % 2 == 0);
+
+        //even row from bottom, inversions odd = solvable
+        if (even && inversions % 2 != 0 )
+                    isSolvable = true;
+
+        //odd row from bottom, inversions even = solvable
+        if (!even && inversions % 2 == 0){
+                    isSolvable = true;
+        }
+        System.out.println(isSolvable);
+        return isSolvable;
     }
 
     public void countMoves(){
